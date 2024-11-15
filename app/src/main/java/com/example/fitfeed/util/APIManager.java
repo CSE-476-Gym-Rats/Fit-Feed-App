ed7dac0fe1a783869f1805f54939af1013b92dd3
jland29 <lander10@msu.edu>
2024-11-15 05:28:27 -0600

    Post loading simulated
    Simulated post loading to handle what loading posts would look like when friends and workouts saving are implemented.

--- app/src/main/java/com/example/fitfeed/util/APIManager.java
+++ app/src/main/java/com/example/fitfeed/util/APIManager.java
@@ -9,8 +9,17 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import com.example.fitfeed.models.Workout;
+import com.example.fitfeed.models.Post;
+//import com.example.fitfeed.util.TokenManager;
 
 /**
  * util for making API calls
@@ -25,6 +34,8 @@ public class APIManager {
     static final String REGISTER_ENDPOINT = "/register";
     static final String ADD_WORKOUT_ENDPOINT = "/workout";
     static final String PULL_WORKOUTS_ENDPOINT = "/workouts";
+    static final String MAKE_POST_ENDPOINT = "/makepost";
+    static final String GET_POST_ENDPOINT = "/getposts";
 
     /**
      * Send a login request
@@ -89,5 +100,107 @@ public class APIManager {
      */
     public static Boolean Register() {
         return true;
+    }
+
+
+    // Temporary testing lines. Friends are not implemented, so I have no way to test getting posts from friends
+    // Also awaiting approval for changes to backend for Post support
+    private static List<String> testUserFriends = new ArrayList<>();
+    private static Dictionary<String, List<Post>> testPosts = new Hashtable<>();
+
+    /**
+     * Sets up data to simulate added friends and workout posts to load.
+     */
+    public static void setupTestPosts() {
+        testUserFriends.add("friend1");
+        testUserFriends.add("friend20");
+
+        Workout w1 = new Workout();
+        w1.addExercise("Push Ups", 5, 8, 10f);
+        w1.addExercise("Bicep Curls", 5, 10, 50f);
+
+        Workout w2 = new Workout();
+        w2.addExercise("Squats", 3, 5, 200f);
+
+        Workout w3 = new Workout();
+        w3.addExercise("Bench", 1, 5, 150f);
+
+        Workout w4 = new Workout();
+        w4.addExercise("lazy", 10, 0, 15f);
+
+        PostWorkout("friend1", "just did my first workout!", w1, null);
+        PostWorkout("friend1", "just benched 150!", w3, null);
+        PostWorkout("friend20", "Leg day today", w2, null);
+
+        // Non-friend
+        PostWorkout("friend0", "Not friend testing", w4, null);
+    }
+
+    /**
+     * Get workout posts from user's friends.
+     */
+    public static ArrayList<Post> GetWorkoutPosts() {
+        /*executorService.submit(() -> {
+            int statusCode = 0; // Default to failure
+
+            try {
+                URL url = new URL(API_URL + PULL_WORKOUTS_ENDPOINT);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Content-Type", "application/json");
+            } catch (Exception e) {
+                Log.e("TAG", e.toString());
+                statusCode = -1;
+            }
+        });*/
+
+        ArrayList<Post> result = new ArrayList<>();  // posts to load
+        List<String> friends = new ArrayList<>();  // List of friends
+        Enumeration<String> globalUsers = testPosts.keys();
+
+        // search thru database of users to find friends
+        while (globalUsers.hasMoreElements()) {
+            String nextUser = globalUsers.nextElement();
+            if (testUserFriends.contains(nextUser)) {
+                friends.add(nextUser);
+            }
+        }
+
+        // search thru friends to get posts to load
+        for (String friend : friends) {
+            if (testPosts.get(friend) != null) {  // if the friend has posts
+                // get their posts and put into result!
+                result.addAll(testPosts.get(friend));
+            }
+        }
+
+        // return list of posts to load
+        return result;
+    }
+
+    /**
+     * Create a post (currently simulated due to github request not yet approved)
+     * @param text the post's text
+     * @param workout workout that goes with the post
+     */
+    public static void PostWorkout(String user, String text, Workout workout, String img) {
+        // create a Post that includes a workout, sample user ID/username, and text
+        //String user = TokenManager.getAccessToken();
+
+        // todo: add image support to post
+
+        Post newPost = new Post(text, user, img, workout);
+
+        // add post to test dictionary (will be database when friends implemented)
+        if (testPosts.get(user) != null) {
+            // if user exists, add the post to them
+            testPosts.get(user).add(newPost);
+        }
+        else {
+            // else, add user and posts to dictionary
+            List<Post> l = new ArrayList<>();
+            l.add(newPost);
+            testPosts.put(user, l);
+        }
     }
 }
